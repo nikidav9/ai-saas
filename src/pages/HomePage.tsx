fcf90f2d35fde2c8156c8394bd729f6be9ec199a
nikidav9 <nikidav9@gmail.com>
2026-06-03 00:21:11 +0300

    Add HomePage component with welcome banner

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './HomePage.css';
+
+const HomePage: React.FC = () => (
+  <section className="home">
+    <h1 className="home__title">Добро пожаловать в AI SaaS</h1>
+    <p className="home__subtitle">Интеллектуальные решения для вашего бизнеса</p>
+  </section>
+);
+
+export default HomePage;
