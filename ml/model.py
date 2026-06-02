9c9d890cdb5f5072982192d4515905b7ba80b482
nikidav9 <nikidav9@gmail.com>
2026-06-03 00:44:39 +0300

    Update model with repo link

--- ml/model.py
+++ ml/model.py
@@ -1,11 +1,8 @@
 REPO_URL = "https://github.com/nikidav9/ai-saas"
 
 class Model:
-    def __init__(self, config):
-        self.config = config
-    def train(self, data):
-        # training logic placeholder
-        pass
-    def predict(self, input_data):
-        # prediction logic placeholder
-        pass
+    def __init__(self, version="v1"):
+        self.version = version
+    def predict(self, x):
+        # placeholder prediction
+        return x
