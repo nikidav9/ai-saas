fef3228b25c02d1c08472aa93d22d6202bf25af6
nikidav9 <nikidav9@gmail.com>
2026-06-02 23:28:04 +0300

    Add tokens.css import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
-import '@/styles/tokens.css';
-import '@/styles/globals.css';
-import { ThemeProvider } from '@/components/ThemeProvider';
+import '../styles/tokens.css';
+import { ThemeProvider } from '@mui/material/styles';
 import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
 
