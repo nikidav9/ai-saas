f10f1928a9fae4e5c207044bb63a192361250472
nikidav9 <nikidav9@gmail.com>
2026-06-03 00:44:00 +0300

    Add model with repo link

--- ml/model.py
+++ ml/model.py
@@ -1,12 +1,11 @@
-import joblib
-
 REPO_URL = "https://github.com/nikidav9/ai-saas"
 
 class Model:
-    def __init__(self, path="model.pkl"):
-        self.path = path
-        self.model = joblib.load(path)
-        self.version = "v2.0"
-
-    def predict(self, X):
-        return self.model.predict(X)
+    def __init__(self, config):
+        self.config = config
+    def train(self, data):
+        # training logic placeholder
+        pass
+    def predict(self, input_data):
+        # prediction logic placeholder
+        pass
