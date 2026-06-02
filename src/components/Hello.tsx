1a32ba38b03354fdaa9192dd4735aaa0f25600a7
nikidav9 <nikidav9@gmail.com>
2026-06-03 02:39:56 +0300

    cleanup: remove agent-generated garbage file src/components/Hello.tsx

--- src/components/Hello.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { getGreeting } from '../utils/greeting';
-
-const Hello: React.FC = () => (
-  <div className="p-4 text-center text-xl font-bold">{getGreeting()}</div>
-);
-
-export default Hello;
